55f0a69b338cd175d998fffeceeb6668161f55b0
Shamlen Pillay <Shamlen.Pillay@media24.com>
2019-08-19 10:36:20 +0200

    #3-CategoryType CRUD Pages Added; also Partial Views for Index Page Edit, Details and Delete Buttons.

---
+++ RestaurantWebApp/RestaurantWebApp/Pages/CategoryTypes/Details.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RestaurantWebApp.Data;
+
+namespace RestaurantWebApp.Pages.CategoryTypes
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DetailsModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public CategoryType categoryType { get; set; }
+
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            categoryType = await _db.CategoryType.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (categoryType == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+    }
+}
